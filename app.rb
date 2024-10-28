2ffe12a44028bf3b5b7356c55aa644d50fa865b6
Spencer Dearman <121642493+spencerdearman@users.noreply.github.com>
2024-10-28 03:50:55 +0000

    finished

--- app.rb
+++ app.rb
@@ -1,9 +1,66 @@
 require "sinatra"
 require "sinatra/reloader"
 
+# 0 == rock, 1 == paper, 2 == scissors
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+def move_name(move)
+  if move == 0
+    return "rock"
+  elsif move == 1
+    return "paper"
+  else
+    return "scissors"
+  end
+end
+
+get("/rock") do
+  our_move = 0
+  @our_move_text = move_name(our_move)
+  their_move = rand(3)
+  @their_move_text = move_name(their_move)
+  if their_move == 1
+    @outcome = 1
+  elsif their_move == 0
+    @outcome = -1
+  else
+   @outcome = 0
+  end
+
+  erb(:game)
+end
+
+get("/paper") do
+  our_move = 1
+  @our_move_text = move_name(our_move)
+  their_move = rand(3)
+  @their_move_text = move_name(their_move)
+  if their_move == 0
+    @outcome = 0
+  elsif their_move == 2
+    @outcome = 1
+  else
+   @outcome = -1
+  end
+
+  erb(:game)
+end
+
+get("/scissors") do
+  our_move = 2
+  @our_move_text = move_name(our_move)
+  their_move = rand(3)
+  @their_move_text = move_name(their_move)
+  if their_move == 0
+    @outcome = 1
+  elsif their_move == 1
+    @outcome = 0
+  else
+   @outcome = -1
+  end
+
+  erb(:game)
 end
